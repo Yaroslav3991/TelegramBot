94c616bbcb3a9fddd1595681ce82d529cd3e40b6
yaroslav3991 <yaroslav3991@gmail.com>
2020-10-04 10:51:32 +0300

    Add Fact

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ Example
 
 Example
 /crypto btc
+
+3. Give a Fact
+/fact
+
+If Fact repeat, you can update it
+/update facts
+
+
 `;
 
 bot.onText(/\/start/i, (msg, match) => {
@@ -55,23 +63,10 @@ bot.onText(/\/start/i, (msg, match) => {
   bot.sendMessage(userId, "Greetings traveler! " + helpMessage);
 });
 
-bot.onText(/\/help/i, (msg, match) => {
-  const userId = msg.chat.id;
-
-  bot.sendMessage(userId, helpMessage);
-});
-
 bot.onText(/help/i, (msg, match) => {
   const userId = msg.chat.id;
 
   bot.sendMessage(userId, helpMessage);
-});
-
-bot.onText(/\/echo (.+)/i, (msg, match) => {
-  const userId = msg.chat.id;
-  const resp = match[1];
-
-  bot.sendMessage(userId, resp);
 });
 
 bot.onText(/\/rates (.+)/i, (msg, match) => {
@@ -121,11 +116,77 @@ Market Cap: ${data.MKTCAP}
 
     bot.sendMessage(userId, message);
   } catch (err) {
-    console.log(err);
+    console.log(err, "117");
     bot.sendMessage(userId, "Error, try again later");
   }
 });
 
+//Data store from gsheet
+let dataStore = [];
+
+getData();
+
+bot.onText(/fact/i, (msg) => {
+  const userId = msg.chat.id;
+  //get max row number
+  let maxRow = dataStore.filter((item) => {
+    return item.row == "1" && item.col == "2";
+  })[0].val;
+
+  //generate random number from 1 to max row
+  let k = Math.floor(Math.random() * maxRow) + 1;
+  //get fact object that matches row with randomly generated number
+  let fact = dataStore.filter((item) => {
+    return item.row == k && item.col == "5";
+  })[0];
+
+  //output message
+  let message = `
+Fact #${fact.row}:
+${fact.val}
+  `;
+  //reply user
+  bot.sendMessage(userId, message);
+});
+
+bot.onText(/update/i, async (msg) => {
+  const userId = msg.chat.id;
+
+  try {
+    //update data
+    await getData();
+    bot.sendMessage(userId, "updated");
+  } catch (err) {
+    console.log(err, "161");
+    bot.sendMessage(userId, "Error encountered");
+  }
+});
+
+async function getData() {
+  try {
+    //send http request to gs link to get information back in json format
+    let res = await axios(
+      "https://spreadsheets.google.com/feeds/cells/1qwunC72mqNN2Vfy2tIiOrwpxOnHn3AnWmLfsf18llIA/1/public/full?alt=json"
+    );
+
+    //store entry array into data variable
+    let data = res.data.feed.entry;
+    //make sure dataStore is empty
+    dataStore = [];
+    //process data into dataStore
+    data.forEach((item) => {
+      dataStore.push({
+        row: item.gs$cell.row,
+        col: item.gs$cell.col,
+        val: item.gs$cell.inputValue,
+      });
+    });
+  } catch (err) {
+    console.log(err, "186");
+    throw new Error();
+  }
+}
+
 bot.on("message", (msg) => {
-  console.log(msg);
+  // console.log(msg);
 });
